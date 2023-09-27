8463313ab19fa7e04954541ade5a5eeabeb211e4
nish-on <nish.jobmail@gmail.com>
2023-09-27 18:13:05 +0200

    003: Split Text after amount

--- src/main/java/com/github/nish_on/salestaxes/SalesTaxes.java
+++ src/main/java/com/github/nish_on/salestaxes/SalesTaxes.java
@@ -1,2 +1,15 @@
-package com.github.nish_on.salestaxes;public class SalesTaxes {
+package com.github.nish_on.salestaxes;
+
+
+
+public class SalesTaxes {
+
+
+    public String[] getAmountAndRestOfItemDescription(String itemDescription) {
+
+        String[] firstSpaceSplitted = itemDescription.split(" ", 2);
+        return firstSpaceSplitted;
+
+    }
+
 }
---
+++ src/test/java/com/github/nish_on/salestaxes/SalesTaxesTest.java
@@ -0,0 +1,21 @@
+package com.github.nish_on.salestaxes;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+public class SalesTaxesTest {
+
+    SalesTaxes salesTaxes = new SalesTaxes();
+
+    @Test
+    public void testFirstSplitSuccess(){
+        assertThat(salesTaxes.getAmountAndRestOfItemDescription("1 book at 12.49")).isEqualTo(new String []{"1", "book at 12.49"});
+    }
+
+    @Test
+    public void testFirstSplitFails(){
+        assertThat(salesTaxes.getAmountAndRestOfItemDescription("1 book at 12.49")).isNotEqualTo(new String []{"1", "book", "at", "12.49"});
+    }
+
+}
